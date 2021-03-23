307f19cbce747fd97c679c195b27392ca7001048
roca12 <roca230313@gmail.com>
2021-03-22 20:17:57 -0500

    Solucionado el problema de la jdbc en la persistencia y en el servidor, adema se recrearon las paginas JSF de cada tabla

---
+++ src/main/java/com/artemispocket/beans/AuditoriaAccesoFacade.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.artemispocket.beans;
+
+import com.artemispocket.entities.AuditoriaAcceso;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author roca12
+ */
+@Stateless
+public class AuditoriaAccesoFacade extends AbstractFacade<AuditoriaAcceso> {
+
+    @PersistenceContext(unitName = "com.roca12artemis_MiniArtemisMaven_war_1.0-SNAPSHOTPU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public AuditoriaAccesoFacade() {
+        super(AuditoriaAcceso.class);
+    }
+    
+}
